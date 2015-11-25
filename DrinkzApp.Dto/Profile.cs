21b6de0780694c357f3613ec1d86e967c35248a3
João Paulo Amorim Rosa <joaopaulo18@live.it>
2015-11-25 18:07:03 -0200

    Added Profile DTO
    DataTable Object for Profile

---
+++ DrinkzApp.Dto/Profile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkzApp.Dto
+{
+    public class Profile
+    {
+        public int PK_PROFILE { get; set; }
+
+        public string FACEBOOK_ID { get; set; }
+
+        public string NAME { get; set; }
+
+        public string URL_IMAGE { get; set; }
+
+        public int FK_ADDRESS { get; set; }
+    }
+}
